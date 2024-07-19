3b5bdfe124eccdefdc5b19febfbf591ab935b0b3
Furtido <166959167+Furtido@users.noreply.github.com>
2024-07-18 21:02:46 -0500

    finish and testing

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project in javascript about the game rock, paper or scissors
--- javascript/java.js
+++ javascript/java.js
@@ -15,3 +15,45 @@ function getComputerChoice(){
     console.log(computer)
 return computer;
 }
+
+function getHumanChoice(){
+    let human = prompt("what do u choose: rock, paper or scissors");
+    while (human == null) {
+        human = prompt("what do u choose: rock, paper or scissors");
+    }
+    human = human.toLocaleLowerCase();
+    console.log(human);
+return human
+}
+
+function PlayRound(){
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    const winner = ChooseWinner(humanSelection, computerSelection);
+    console.log(winner)
+}
+
+function ChooseWinner(humanChoice, computerChoice){
+    if (computerChoice === humanChoice){
+        Draw++;
+        return "Draw";
+        
+    }else if ((humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "rock" && computerChoice === "scissors")){
+        humanScore++;
+        return "Player win";
+
+    }else {
+        ComputerScore++;
+        return "Computer win";
+
+    }
+}
+
+function PlayGame(){
+    for (let i = 0; i < 5; i++){
+        PlayRound();
+    }
+}
+
+PlayGame();
+console.log("Your score is: " + humanScore + " Computer score is: " + ComputerScore + " Draw games: " + Draw );
